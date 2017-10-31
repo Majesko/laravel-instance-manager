a617da283eb894f0a9fddedb7c44208fa280a66b
Majesko <majesko@gmail.com>
2017-10-31 17:08:27 +0300

    initial commit

---
+++ src/LaravelInstanceManager.php
@@ -0,0 +1,51 @@
+<?php
+
+
+use Illuminate\Support\Facades\Config;
+use InstanceManager\AdapterInterface;
+use InstanceManager\AWS\AwsAdapter;
+use InstanceManager\InstanceManager;
+
+class LaravelInstanceManager implements AdapterInterface {
+	private $config;
+	private $defaultProvider;
+	private $adapters;
+
+	public function __construct() {
+		$this->adapters = [
+			'aws' => AwsAdapter::class
+		];
+		$this->config = Config::get('instance-manager::instance-manager');
+		$this->defaultProvider = $this->config[$this->config['default']];
+	}
+
+	public function boot($provider) {
+
+		if (array_key_exists($provider, $this->config) && array_key_exists($provider, $this->adapters)) {
+
+			$adapter = new $this->adapters[$provider]($this->config[$provider]);
+
+			return new InstanceManager($adapter);
+		}
+
+		throw new AdapterNotFoundException();
+	}
+
+	public function createInstances($count) {
+		$adapter = new $this->adapters[$this->defaultProvider]($this->config[$this->defaultProvider]);
+
+		return (new InstanceManager($adapter))->createInstances($count);
+	}
+
+	public function describeInstances($names) {
+		$adapter = new $this->adapters[$this->defaultProvider]($this->config[$this->defaultProvider]);
+
+		return (new InstanceManager($adapter))->describeInstances($names);
+	}
+
+	public function terminateInstances($names) {
+		$adapter = new $this->adapters[$this->defaultProvider]($this->config[$this->defaultProvider]);
+
+		return (new InstanceManager($adapter))->terminateInstances($names);
+	}
+}
---
+++ src/config/instance-manager.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'default' => 'aws',
+
+	'aws' => [
+		'region' => '',
+		'version' => 'latest',
+		'key' => '',
+		'secret' => '',
+		'instance-type' => '',
+		'image-id' => '',
+		'key-name' => '',
+		'security-groups' => []
+	],
+];
